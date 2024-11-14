d66a2f032cf5b4d893b1633c2cc1bac63e32a28d
walkvlm2024 <walkvlm2024@gmail.com>
2024-11-14 16:22:54 +0800

    update

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-.video-container {
-    display: flex; /* 使用Flexbox布局 */
-    overflow-x: auto; /* 允许水平滚动 */
-    gap: 10px; /* 设置视频之间的间隙为0 */
+
+
+
+
+
+
+
+
+{
+    "video": "20240918-youtube_short_e93770538101c9669e57265fe378776e_1m42s.mp4",
+    "frame_path": "20240918-youtube_short_e93770538101c9669e57265fe378776e_1m42s.frame",
+    "weather_condition": "Sunny",
+    "area_type": "Pedestrian Path",
+    "danger_level": "Mid",
+    "traffic_flow_rating": "Low",
+    "summary": "on the sidewalk on the right side of the road, there is a downward step on the left, a yellow car passes on the left side of the road, there is a row of green plants on the right, there is a row of trees at one o'clock direction, there is a sign at two o'clock direction, there are cars parked on the roadside at eleven o'clock direction, the current road is narrow and there are few pedestrians",
+    "alter": "at 11 o'clock direction there is a car, at 1 o'clock direction there is a sign, be careful to avoid."
 }
-
-.video-item {
-    flex: 0 0 60%; /* 每个项目占据30%的宽度，并且不伸缩 */
-}
-
-.video-item video {
-    width: 100%; /* 视据宽度占满父容器 */
-    height: auto; /* 高度自适应 */
-}
-
-
